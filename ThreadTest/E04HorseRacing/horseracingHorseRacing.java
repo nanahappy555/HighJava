5be63b35b0577484b600f2e0dc537cab82fb6644
RIM <80573832+nanahappy555@users.noreply.github.com>
2022-05-26 01:59:40 +0900

    Update and rename ThreadTest/exhorseracing/HorseRacing.java to ThreadTest/E04HorseRacing/horseracingHorseRacing.java

--- ThreadTest/E04HorseRacing/horseracingHorseRacing.java
+++ ThreadTest/E04HorseRacing/horseracingHorseRacing.java
@@ -6,19 +6,20 @@ import java.util.List;
 import java.util.Random;
 
 public class HorseRacing {
-//	static String strRank="";
 
 	public static void main(String[] args) {
-		
+
 		System.out.println();
 		System.out.println("경기 시작");
+		
+		//텀을 위한 sleep() 큰 의미 없음
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e2) {
 			e2.printStackTrace();
 		}
 
-//		
+		//ArrayList로 말 추가. 스레드 생성
 		List<Horse> hList = new ArrayList<Horse>();
 
 		hList.add(new Horse("01번말"));
@@ -32,40 +33,38 @@ public class HorseRacing {
 		hList.add(new Horse("09번말"));
 		hList.add(new Horse("10번말"));
 
-		Position p = new Position(hList);
-		
+		//말 위치 출력 스레드 생성
+		Thread p = new Position(hList);
+
+		//foreach문으로 모든 말 스레드 실행 (10개스레드)
 		for (Thread th : hList) {
 			th.start();
 		}
+		
+		//말 위치 출력 스레드 실행
 		p.start();
-		
-		
-		for (Thread th : hList) {
+
 			try {
-				th.join();
 				p.join();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}
 		try {
-			Thread.sleep(500); //0.2초씩 출력 텀
+			Thread.sleep(1000); // 1초 멈췄다가 다음코드 실행
 		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		Collections.sort(hList);
 		System.out.print("\n\n\n");
 		System.out.println("------------------------");
 		System.out.println("경기 끝");
 		System.out.println("------------------------");
 		System.out.println("경기 결과");
-		System.out.println("축하합니다! 1등은 " + hList.get(0).getHName() +  " 입니다.\n");
+		System.out.println("축하합니다! 1등은 " + hList.get(0).getHName() + " 입니다.\n");
+		//Horse객체(toString)출력
 		for (Horse h : hList) {
 			System.out.println(h);
-//			System.out.printf("%d등 : %s \n", h.getRank(), h.getHName());
 		}
-
 	}
 }
