b2a966973dfa78f05a0f9cca41736fe9fbcbb29a
RIM <80573832+nanahappy555@users.noreply.github.com>
2022-06-09 09:03:14 +0900

    Add files via upload

--- JavaMVCTest/member/dao/MemberDAOImpl.java
+++ JavaMVCTest/member/dao/MemberDAOImpl.java
@@ -191,9 +191,40 @@ boolean isExist = false;
 			conn = JDBCUtil3.getConnection();
 			
 			//동적쿼리 (어떤 값을 입력하냐에 따라 다이나믹하게 바뀜
-			String sql = "";
+			String sql = "select * from mymember where 1=1 "; //항상 들어가는 sql문
+			
+			if(mv.getMemId() != null && !mv.getMemId().equals("")) {
+				//memId가 not null && not "" empty string
+				sql += " and mem_id = ? "; //sql문을 누적
+			}
+			if(mv.getMemName() != null && !mv.getMemName().equals("")) {
+				sql += " and mem_name = ? ";
+			}
+			if(mv.getMemTel() != null && !mv.getMemTel().equals("")) {
+				sql += " and mem_tel = ? ";
+			}
+			if(mv.getMemAddr() != null && !mv.getMemAddr().equals("")) {
+				sql += " and mem_addr like '%' || ? || '%' ";
+			}
+			//최종 쿼리문 확인용
+			System.out.println("최종 SQL : " + sql);
 			
 			pstmt = conn.prepareStatement(sql);
+			
+			int index = 1; //물음표 위치 지정.(sql은 1부터 count) ?갯수도 다이나믹하고 index도 유동적으로 바뀐다.
+			if(mv.getMemId() != null && !mv.getMemId().equals("")) {
+				pstmt.setString(index++, mv.getMemId());
+			}
+			if(mv.getMemName() != null && !mv.getMemName().equals("")) {
+				pstmt.setString(index++, mv.getMemName());
+			}
+			if(mv.getMemTel() != null && !mv.getMemTel().equals("")) {
+				pstmt.setString(index++, mv.getMemTel());
+			}
+			if(mv.getMemAddr() != null && !mv.getMemAddr().equals("")) {
+				pstmt.setString(index++, mv.getMemAddr());
+			}
+			
 			
 			rs = pstmt.executeQuery();
 			
@@ -203,7 +234,7 @@ boolean isExist = false;
 				String memTel = rs.getString("mem_tel");
 				String memAddr = rs.getString("mem_addr");
 				
-				//뽑아온 정보를 VO에 담아줌
+				//뽑아온 정보를 VO에 담아줌. mv2인 이유: 매개변수가 mv라서. mv는 사용자가 검색을 위해 입력한 값
 				MemberVO mv2 = new MemberVO();
 				mv2.setMemId(memId);
 				mv2.setMemName(memName);
