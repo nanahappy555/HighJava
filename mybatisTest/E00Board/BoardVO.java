f8c63b4d25875fa68dcba61c9850cc2ce2099dc9
RIM <80573832+nanahappy555@users.noreply.github.com>
2022-06-18 23:20:35 +0900

    Update BoardVO.java

--- mybatisTest/E00Board/BoardVO.java
+++ mybatisTest/E00Board/BoardVO.java
@@ -1,4 +1,49 @@
+package mybatisboard;
 
 public class BoardVO {
-  
+	private int bNo;
+	private String bTitle;
+	private String bWriter;
+	private String bContent;
+	private String bDate;
+	private String bEndDate; //검색
+	public int getbNo() {
+		return bNo;
+	}
+	public void setbNo(int bNo) {
+		this.bNo = bNo;
+	}
+	public String getbTitle() {
+		return bTitle;
+	}
+	public void setbTitle(String bTitle) {
+		this.bTitle = bTitle;
+	}
+	public String getbWriter() {
+		return bWriter;
+	}
+	public void setbWriter(String bWriter) {
+		this.bWriter = bWriter;
+	}
+	public String getbContent() {
+		return bContent;
+	}
+	public void setbContent(String bContent) {
+		this.bContent = bContent;
+	}
+	public String getbDate() {
+		return bDate;
+	}
+	public void setbDate(String bDate) {
+		this.bDate = bDate;
+	}
+	public String getbEndDate() {
+		return bEndDate;
+	}
+	public void setbEndDate(String bEndDate) {
+		this.bEndDate = bEndDate;
+	}
+	
+	
+	
 }
